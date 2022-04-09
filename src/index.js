ab44779e40aa813f3cdbe8a95762a5939eb78a3a
Felipe Navas <felipenavas.itec@gmail.com>
2022-04-09 16:50:12 -0300

    Upgrading to React 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+// Before ------------------------------------------------
+/**
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import PrimeraApp from './PrimeraApp'
 import CounterApp from './CounterApp';
-
 import './index.css';
-
 const divRoot = document.querySelector('#app');
-
 //ReactDOM.render( <PrimeraApp saludo='Hola' subtitulo='Subtitulo enviado desde Index' />  , divRoot );
-ReactDOM.render( <CounterApp value={ 10 } />  , divRoot );
+ReactDOM.render(<CounterApp value={10} />, divRoot);
+*/
+
+// After React 18 ------------------------------------------------
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CounterApp from './CounterApp';
+import './index.css';
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<CounterApp />);
